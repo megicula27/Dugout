c54fc81d0c69f5470a44a68ceaef1a3dccbc5558
megicula27 <aditya1272001@gmail.com>
2024-11-07 01:44:52 +0530

    added form validation

--- src/schema/validationSchema.js
+++ src/schema/validationSchema.js
@@ -2,15 +2,21 @@ import * as Yup from "yup";
 
 export const SignupSchema = Yup.object().shape({
   username: Yup.string()
-    .min(6, "Username must be at least 6 characters")
+    .matches(
+      /^[a-zA-Z0-9]{6,}$/,
+      "Username must be at least 6 alphanumeric characters"
+    )
     .required("Username is required"),
   email: Yup.string()
-    .email("Invalid email address")
+    .matches(
+      /^([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])$/,
+      "Invalid email format"
+    )
     .required("Email is required"),
   password: Yup.string()
-    .min(8, "Password must be at least 8 characters")
-    .matches(/[a-z]/, "Password must contain at least one lowercase letter")
-    .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
-    .matches(/[0-9]/, "Password must contain at least one number")
+    .matches(
+      /^(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/,
+      "Password must be at least 8 characters with one special character"
+    )
     .required("Password is required"),
 });
